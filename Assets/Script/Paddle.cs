62037e1db9c5c737fa65cbc4ab5a029817417cb3
Hopi <89178015+Hopiboi@users.noreply.github.com>
2023-10-25 04:03:28 +0800

    Pong Recreation

---
+++ Assets/Script/Paddle.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    protected Rigidbody2D rg2D;
+
+    public float paddleSpeed = 10f;
+
+    private void Awake()
+    {
+        rg2D = GetComponent<Rigidbody2D>();
+    }
+
+
+}
