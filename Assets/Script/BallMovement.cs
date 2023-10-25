b046538eaf0fa59b5400ddc002f3c8d6507af759
Hopi <89178015+Hopiboi@users.noreply.github.com>
2023-10-26 01:08:56 +0800

    Pong Recreation

--- Assets/Script/BallMovement.cs
+++ Assets/Script/BallMovement.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class BallMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] private float ballSpeed = 200f;
+
+    private Rigidbody2D rg2D;
+
+    private void Start()
     {
-        
+        rg2D = GetComponent<Rigidbody2D>();
+
+        StartingSpeed();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void StartingSpeed()
     {
+        //Random in left or right
+        float x = Random.value < 0.5f ? -1f : 1f;
+
+        // Down and Up
+        float y = Random.value < 0.5f ? Random.Range(-1f, -.5f):
+                                        Random.Range(0.5f, 1f); // Angle
         
+        
+        //adding direction
+        Vector2 direction = new Vector2(x, y);
+        rg2D.AddForce(direction * ballSpeed);
+
     }
+
 }
--- Assets/Script/EnemyPaddle.cs
+++ Assets/Script/EnemyPaddle.cs
@@ -4,5 +4,39 @@ using UnityEngine;
 
 public class EnemyPaddle : Paddle
 {
+    //Accessing rigidbody in script in inspector
+    public Rigidbody2D ball;
     
+    private void FixedUpdate()
+    {
+        //if its going to right
+        if (this.ball.velocity.x > 0f)
+        {
+            //if its going to up
+            if(this.ball.position.y > this.transform.position.y)
+            {
+                rg2D.AddForce(Vector2.up * this.paddleSpeed);
+            }
+            else if (this.ball.position.y < this.transform.position.y)
+            {
+                rg2D.AddForce(Vector2.down * this.paddleSpeed);
+            }
+        }
+
+        //variability to make player win
+        else
+        {
+            if(this.transform.position.y > 0f)
+            {
+                rg2D.AddForce(Vector2.down * this.paddleSpeed);
+            }
+            else if (this.transform.position.y < 0f)
+            {
+                rg2D.AddForce(Vector2.up * this.paddleSpeed);
+            }
+        }
+    }
+
+
+
 }
